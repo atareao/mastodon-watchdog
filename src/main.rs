e90904784f3d62c81d7d35f7d448efbe47a9c908
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-09-27 19:15:57 +0200

    minor changes

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 mod twitter;
 mod feedback;
 mod config;
+mod message;
 
 use dotenv::dotenv;
 use std::{thread, time, env};
 use tokio;
-use crate::{twitter::Twitter, config::Config};
+use crate::{twitter::Twitter, config::Config, feedback::Feedback};
 use serde_json::{Map, Value};
+use crate::message::{check_key, check_comment};
 
 const FILENAME: &str = "lastid.toml";
 
@@ -16,12 +18,16 @@ async fn main() {
     dotenv().expect("Not found environment");
     let mut config = Config::read("lastid.toml").expect("Can not read last id");
     let mut last_id = config.get_last_id().to_string();
+    let url = env::var("URL")
+        .expect("Not found URL");
+    let token = env::var("TOKEN")
+        .expect("Not found TOKEN");
     let sleep_time_in_seconds = env::var("SLEEP_TIME")
         .expect("Not found SLEEP_TIME")
         .parse::<u64>()
         .unwrap();
     let consumer_key = env::var("TW_CONSUMER_KEY").expect("Not foun consumer key");
-    let consumer_secret = env::var("TW_CONSUMER_SECRET").expect("Not foun consumer secret");
+    let consumer_secret = env::var("TW_CONSUMER_SECRET").expect("Not found consumer secret");
     let access_token = env::var("TW_ACCESS_TOKEN").expect("Not found access token");
     let access_token_secret = env::var("TW_ACCESS_TOKEN_SECRET").expect("Not found access token secret");
     let sleep_time = time::Duration::from_secs(sleep_time_in_seconds);
@@ -64,7 +70,7 @@ async fn main() {
     }
 }
 
-async fn search(twitter: &Twitter, last_id: &str) -> Option<String>{
+async fn search(url: &str, token: &str, twitter: &Twitter, last_id: &str) -> Option<String>{
     let mut new_last_id: String = "".to_string();
     let res = &twitter.get_mentions(&last_id).await;
     if res.is_ok(){
@@ -85,6 +91,26 @@ async fn search(twitter: &Twitter, last_id: &str) -> Option<String>{
             println!("created_at: {}", created_at);
             println!("Name: {}", name);
             println!("Screen Name: {}", screen_name);
+            if let Some(message) = check_key("idea", text){
+                let feedback = Feedback::new("idea", &new_last_id, text, name, screen_name, 0, "Twitter");
+                feedback.post(url, token);
+            }else if let Some(message) = check_key("pregunta", text){
+                let feedback = Feedback::new("pregunta", &new_last_id, text, name, screen_name, 0, "Twitter");
+                feedback.post(url, token);
+            }else if let Some(option) = check_comment("comentario", text){
+                let (commentario, reference) = option;
+                if let Some(message) = commentario{
+                    let id = match reference {
+                        Some(value) => value,
+                        None => new_last_id.clone(),
+                    };
+                    let feedback = Feedback::new("comentario", &id, &message, name, screen_name, 0, "Twitter");
+                    feedback.post(url, token);
+                }
+            }else{
+                let feedback = Feedback::new("mencion", &new_last_id, text, name, screen_name, 0, "Twitter");
+                feedback.post(url, token);
+            }
         }
     }
     if new_last_id != "" && new_last_id != last_id{
